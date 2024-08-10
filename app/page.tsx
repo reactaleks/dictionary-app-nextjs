3281463ba9ef39e4c36bf30dbef33ff5507e07a0
Aleks <a.ivanov.developer@gmail.com>
2024-08-10 23:49:46 +0100

    Added basic form input, and a server action to fetch data based on user input.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { getDictionaryData } from "@/components/ServerActions";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
- 
+        <form action={getDictionaryData}>
+            <input type='text' name="searchTerm"/>
+            <button type="submit">Submit form</button>
+        </form>
     </main>
   );
 }
---
+++ components/ServerActions.tsx
@@ -0,0 +1,12 @@
+'use server'
+
+export async function getDictionaryData(formData: FormData) {
+    // Get input field data - user search parameter
+    const rawFormData = {
+        dictionarySearchTerm: formData.get('searchTerm'),
+    }
+    // Fetch data based on user search parameter
+    const dictionaryData = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/` + `${rawFormData.dictionarySearchTerm}`).then((res) => res.json()).catch((err) => console.log(err))
+
+    console.log(dictionaryData);
+}
