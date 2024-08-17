96391ba9e27595c9fb8465ec514444de2466bb53
Aleks <a.ivanov.developer@gmail.com>
2024-08-17 22:37:17 +0100

    delete packalock from npm as using yarn rework data output component

--- components/output_component/OutputComponent.tsx
+++ components/output_component/OutputComponent.tsx
@@ -3,7 +3,7 @@ import DataOutput from "./data_ouput_components/DataOutput";
 
 export default function Output({ apiresponse }: OutputProps) {
   const errorMessage = apiresponse?.data?.message
-  
+  console.log(apiresponse)
   if (errorMessage) {
     return <ErrorOutput apiresponse={apiresponse} />;
   } 
